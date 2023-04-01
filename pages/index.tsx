9e7a36bd5b9ec41353dafe9ce6c653caccf04aba
birrellc <vaiezmedia@gmail.com>
2023-04-01 15:16:42 +0100

    Feat: added slider to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import Slider from '@/components/Slider';
 import Head from 'next/head';
 import { useEffect, useRef, useState } from 'react';
 
@@ -11,8 +12,9 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='uppercase'>
+      <div className='uppercase text-[#010101]'>
         <Header />
+        <Slider />
       </div>
     </>
   );
