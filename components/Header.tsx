abfeffc0c10e37940d21188b8803be1018812afc
birrellc <vaiezmedia@gmail.com>
2023-04-01 15:16:26 +0100

    Feat: finished layout of mobile and desktop navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,28 +6,57 @@ import menu from '../assets/menu-outline.svg';
 const Header = () => {
   return (
     <div>
-      <div className='absolute bg-black top-0 py-[5px] h-[20vh] w-[100%] flex justify-between flex-col text-white'>
-        <nav
+      <nav className='fixed w-full top-0 left-0 overflow-hidden h-screen-translate-y-[1200px]'>
+        <div className='flex items-start justify-between w-[100%] h-fit py-[5px]'>
+          <div className='mx-[5px] font-semibold text-[20px] text-red leading-none'>
+            CB
+          </div>
+          <div className='block md:hidden cursor-pointer mx-[5px]'>
+            <Image
+              src={close}
+              alt='close image'
+              className='w-10 object-contain invert'
+            />
+          </div>
+        </div>
+        <div className='mx-2 my-10 space-y-1 text-white'>
+          <span className='overflow-hidden block'>
+            <div className='font-bold text-[30px]'>ABOUT</div>
+          </span>
+          <span className='overflow-hidden block'>
+            <div className='font-bold text-[30px]'>WORK</div>
+          </span>
+          <span className='overflow-hidden block'>
+            <div className='font-bold text-[30px]'>PUBLICATIONS</div>
+          </span>
+          <span className='overflow-hidden block'>
+            <div className='font-bold text-[30px]'>OFFICE</div>
+          </span>
+        </div>
+      </nav>
+      <nav className='absolute bg-white top-0 py-[5px] h-[20vh] w-[100%] flex justify-between flex-col text-black'>
+        <div
           id='header'
           className='flex items-start justify-between w-[100%] h-fit'
         >
-          <div className='mx[-5px] mix-blend-difference font-semibold text-[20px] leading-none'>
+          <div className='mx[-5px] font-semibold text-[20px] leading-none'>
             CB
           </div>
-          <div className='mx-1 md:block hidden mix-blend-difference font-bold text-[30px]'>
+          <div className='mx-1 md:block hidden font-bold text-[30px]'>
             ABOUT
           </div>
-          <div className='mx-1 md:block hidden mix-blend-difference font-bold text-[30px]'>
-            WORK
-          </div>
-          <div className='mx-1 md:block hidden mix-blend-difference font-bold text-[30px]'>
+          <div className='mx-1 md:block hidden font-bold text-[30px]'>WORK</div>
+          <div className='mx-1 md:block hidden font-bold text-[30px]'>
             PUBLICATIONS
           </div>
-          <div className='mx-1 md:block hidden mix-blend-difference font-bold text-[30px]'>
+          <div className='mx-1 md:block hidden font-bold text-[30px]'>
             OFFICE
           </div>
-        </nav>
-      </div>
+          <div className='block md:hidden cursor-pointer mx-5'>
+            <Image alt='' src={menu} className='w-10 object-contain' />
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
